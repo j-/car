2e0336a6067f3140f23621248a33ce15b28f6a9b
j- <j@skeoh.com>
2018-09-17 14:20:41 +1000

    Add Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,27 @@ import ConvertPower from './ConvertPower';
 import ConvertTorque from './ConvertTorque';
 
 const App: React.StatelessComponent = () => (
-	<div className="App">
+	<div className="App container">
+		<br />
+
 		<h1>Car</h1>
 
+		<br />
+		<br />
+
 		<h2>Power</h2>
+
+		<br />
+
 		<ConvertPower />
 
+		<br />
+		<br />
+
 		<h2>Torque</h2>
+
+		<br />
+
 		<ConvertTorque />
 	</div>
 );
