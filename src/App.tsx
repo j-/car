eab27e59c958dc26fcb1093570f2d84006b19d8c
j- <j@skeoh.com>
2018-09-17 11:26:32 +1000

    Add power converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import ConvertPower from './ConvertPower';
 
 const App: React.StatelessComponent = () => (
 	<div className="App">
-		{null}
+		<ConvertPower />
 	</div>
 );
 
---
+++ src/ConvertPower.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import './ConvertPower.css';
+
+interface State {
+	horsepower: number | null;
+	kilowatts: number | null;
+}
+
+const HP_TO_KW_RATIO = 0.746;
+const DEFAULT_HP = 1000;
+const DEFAULT_KW = DEFAULT_HP * HP_TO_KW_RATIO;
+
+export default class ConvertPower extends React.Component<{}, State> {
+	state: State = {
+		horsepower: DEFAULT_HP,
+		kilowatts: DEFAULT_KW,
+	}
+
+	render () {
+		const { horsepower, kilowatts } = this.state;
+		return (
+			<form className="ConvertPower" onSubmit={this.handleFormSubmit}>
+				<div>
+					<label htmlFor="ConvertPower-horsepower">Horsepower</label><br />
+					<input
+						id="ConvertPower-horsepower"
+						type="number"
+						value={horsepower || ''}
+						onChange={this.handleHorsepowerChange}
+					/>
+				</div>
+				<div>
+					<label htmlFor="ConvertPower-kilowatts">Kilowatts</label><br />
+					<input
+						id="ConvertPower-kilowatts"
+						type="number"
+						value={kilowatts || ''}
+						onChange={this.handleKilowattsChange}
+					/>
+				</div>
+			</form>
+		)
+	}
+
+	private handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	}
+
+	private handleHorsepowerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const horsepower = Number(e.currentTarget.value);
+		const kilowatts = this.horsepowerToKilowatts(horsepower);
+		this.setState({
+			horsepower,
+			kilowatts,
+		});
+	}
+
+	private handleKilowattsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const kilowatts = Number(e.currentTarget.value);
+		const horsepower = this.kilowattsToHorsepower(kilowatts);
+		this.setState({
+			horsepower,
+			kilowatts,
+		});
+	}
+
+	private horsepowerToKilowatts (horsepower: number) {
+		return horsepower * HP_TO_KW_RATIO;
+	}
+
+	private kilowattsToHorsepower (kilowatts: number) {
+		return kilowatts / HP_TO_KW_RATIO;
+	}
+}
